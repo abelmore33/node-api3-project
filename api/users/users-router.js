b0518c50b3e763e5baa34391e82a3ab10875ff77
Aaron Belmore <belmoreaaron9@gmail.com>
2023-08-05 05:45:02 -0400

    completed post

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -19,30 +19,35 @@ router.get("/", (req, res) => {
   });
 });
 
-router.get("/:id", validateUserId, (req, res) => {
+router.get("/:id", validateUserId, (req, res, next) => {
   // RETURN THE USER OBJECT
   // this needs a middleware to verify user id
   Users.getById(req.params.id)
     .then((user) => {
       res.status(200).json(user);
     })
-    .catch((err) => {
-      message: "ewewe";
-    });
+    .catch(next);
 });
 
-router.post("/", validateUser, (req, res) => {
+router.post("/", validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-  // Users.insert().then((newUser) => {
-  //   res.status(200).json(newUser);
-  // }).catch(err => )
+  Users.insert(req.body)
+    .then((newUser) => {
+      res.status(200).json(newUser);
+    })
+    .catch(next);
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", validateUserId, validateUser, (req, res, next) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  Users.update(req.params.id, req.body)
+    .then((updatedUser) => {
+      res.status(201).json(updatedUser);
+    })
+    .catch(next);
 });
 
 router.delete("/:id", (req, res) => {
